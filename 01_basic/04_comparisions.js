9fc9255dbdb96ede27e6029573344ccc97afefb5
Shambho2701 <155106566+Shambho2701@users.noreply.github.com>
2023-12-29 17:32:28 +0000

    04_comparision.js

---
+++ 01_basic/04_comparisions.js
@@ -0,0 +1,30 @@
+
+// comparisions of numb to number 
+console.log(2>3);                   // false
+console.log(2<3);                   // true
+console.log(2==3);                 // false
+console.log(2!=3);                // true
+
+
+// comparisions to string type to num type
+
+console.log("2"> 1);   // true cause 2 isconsider as number or boolean 
+console.log("02">1);   // true 
+
+// when we compare any operation in that case the datatype will be same in both.
+// like     string > string,     num > num     not like string> num 
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null >=0);
+
+console.log(undefined>0);
+
+// must check equality in comparisions 
+
+// strict check 
+// === triple equal
+
+console.log("2"===2);  // in this case the the 1st is string data type and 2nd will num data typee
+
+
